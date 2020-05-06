664eb0e14c4b9d6350a5dba19f9c5c0efc851129
AshfatAbegh <mdabegh50@gmail.com>
2020-05-06 21:57:26 +0600

    Array Done

---
+++ Array.js
@@ -0,0 +1,9 @@
+var friendsAge=[21,20,22];
+
+var SumonAge=friendsAge[1];
+
+var position = friendsAge.indexOf(1);
+
+console.log(position);
+friendsAge.push(19);
+console.log(friendsAge);
---
+++ Hello.js
@@ -0,0 +1,6 @@
+var bananaPrice = 12;
+console.log( bananaPrice);
+var name = "Ashfat";
+console.log(name);
+var isRich=true;
+console.log(isRich);
